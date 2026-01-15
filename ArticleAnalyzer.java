3a5bb6548612ac2135f7b46a06e98a9976b866c0
Dominic <dommos26@bergen.org>
2026-01-15 13:59:41 -0500

    donow1/15

--- ArticleAnalyzer.java
+++ ArticleAnalyzer.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
 public class ArticleAnalyzer {
 
     private ArrayList<String> stopWords; //load from FileOperators
@@ -7,15 +9,21 @@ public class ArticleAnalyzer {
 
     public ArticleAnalyzer(){
         stopWords = FileOperator.getStringList("stopwords.txt");
-        System.out.println("Stopword Count" + stopWords.size());
         articles = new ArrayList<>();
+        System.out.println("Stopword Count" + stopWords.size());
         System.out.println("Articles Count "+articles.size());
-    }
+
+        }
 
     public static void main(String[] args){
         ArticleAnalyzer analyzer = new ArticleAnalyzer();
+        ArrayList<String> lines = FileOperator.getStringList("data.txt");
+        String line = lines.get(0);
+        Article a = analyzer.parseJson(line);
+        System.out.println(a);
     }
     public void addStopWord(String word){
+
     }
 
     public void addArticle(Article article){
@@ -23,20 +31,17 @@ public class ArticleAnalyzer {
 
     public Article parseJson(String jsonLine){
         Article result;
-        String text = 
-        Pattern c = Pattern.compile("re");
-        Pattern h = Pattern.compile("re");
-        Pattern d = Pattern.compile("re");
-        Pattern a = Pattern.compile("re");
-        Pattern t = Pattern.compile("re");
-
+        Pattern l = Pattern.compile("\"link\":\\s*\"([^\"]+)\""); 
+        Matcher lm =l.matcher(jsonLine);
+        String lt = lm.find() ? lm.group(1) : "";
+        result = new Article(lt, "", "", "", "", "");
 
 
         return result;
     } 
 
     public String removeStopWords(String text){
-        String result;
+        String result = text;
 
         return result;
     }
