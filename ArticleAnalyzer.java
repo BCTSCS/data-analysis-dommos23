f0f58f134403c9c666f82bdee6ab96c6a6746b1f
Dominic <dommos26@bergen.org>
2026-01-16 13:34:54 -0500

    1/16

--- ArticleAnalyzer.java
+++ ArticleAnalyzer.java
@@ -2,47 +2,62 @@ import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+
 public class ArticleAnalyzer {
 
     private ArrayList<String> stopWords; //load from FileOperators
     private ArrayList<Article> articles; //load from FileOperators json 
 
     public ArticleAnalyzer(){
-        stopWords = FileOperator.getStringList("stopwords.txt");
-        articles = new ArrayList<>();
-        System.out.println("Stopword Count" + stopWords.size());
-        System.out.println("Articles Count "+articles.size());
+        stopWords=FileOperator.getStringList("stopwords.txt");
+        System.out.println("Stop Word count"+stopWords.size());
+        articles=new ArrayList<>();
+        System.out.println("Articles count"+articles.size());
 
-        }
 
-    public static void main(String[] args){
-        ArticleAnalyzer analyzer = new ArticleAnalyzer();
-        ArrayList<String> lines = FileOperator.getStringList("data.txt");
-        String line = lines.get(0);
-        Article a = analyzer.parseJson(line);
-        System.out.println(a);
+
     }
+    public static void main(String[] args) {
+       ArticleAnalyzer riano = new ArticleAnalyzer();
+       ArrayList<String> lines= FileOperator.getStringList("data.txt");
+       String line =lines.get(0);
+       Article a=riano.parseJson(line);
+       System.out.println(a);
+
+       
+
+
+    }
+
     public void addStopWord(String word){
 
     }
 
     public void addArticle(Article article){
+
     }
 
     public Article parseJson(String jsonLine){
+ 
         Article result;
-        Pattern l = Pattern.compile("\"link\":\\s*\"([^\"]+)\""); 
-        Matcher lm =l.matcher(jsonLine);
-        String lt = lm.find() ? lm.group(1) : "";
-        result = new Article(lt, "", "", "", "", "");
+        Pattern l = Pattern.compile("\"link\":\\s*\"([^\"]+)\"");  //regex to extract words
+        Matcher lm =l.matcher(jsonLine); //parameter - line of text
+         String lt = lm.find() ? lm.group(1) : ""; //extract the destined part
+        result=new Article(lt, "", "", "", "","");
 
+    return result;
+}
 
-        return result;
-    } 
-
+ 
     public String removeStopWords(String text){
-        String result = text;
-
+        
+        String result="";
+  
+       
         return result;
-    }
+
+
+    } //remove stop words from Description
+
+
 }
