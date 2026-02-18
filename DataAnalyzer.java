589b7e1336b582036120f877eca6982352900832
Dominic <dommos26@bergen.org>
2026-02-17 20:39:36 -0500

    MP3CW5

--- DataAnalyzer.java
+++ DataAnalyzer.java
@@ -2,7 +2,7 @@ import java.io.*;
 import java.util.ArrayList;
 public class DataAnalyzer {
 
-    public int linearSearch(ArrayList<Integer> numbers, int target){
+    public static int linearSearch(ArrayList<Integer> numbers, int target){
         int index = 0;
         while(index<numbers.size()){
             if(numbers.get(index) == target){
@@ -32,12 +32,42 @@ public class DataAnalyzer {
         return -1;
     }
 
+    public static int reverseList(ArrayList<Integer> numbers, int target){
+        int left = 0;
+        int right = numbers.size() - 1;
+
+        while(left<right){
+            Integer left_side = numbers.get(left);
+            numbers.set(left, numbers.get(right));
+            numbers.set(right, left_side);
+
+            left++;
+            right--;
+        }
+        for(int i=0; i<numbers.size(); i++){
+            if(numbers.get(i).equals(target)){
+                return i;
+            }
+        }
+        return -1;
+    }
+
     public static void main(String[] args){
         ArrayList<Integer> a = FileOperator.getIntList("nums.txt");
         // Calculate time
     long startTime = System.nanoTime(); 
-    int result = binarySearch(a, 10); 
+    // int result = binarySearch(a, 7); 
+    // int result = linearSearch(a, 7);
+    int result = reverseList(a, 7);
+
     long endTime = System.nanoTime(); 
+
+    if(result != -1){
+        System.out.println("Location: " + result);
+    }
+    else{
+        System.out.println("Not found");
+    }
 
 
 
